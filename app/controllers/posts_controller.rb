d139e59ed0529f3b4cb792ec451e99bf2b40a4a9
kaito8958 <shimo.oka8958@gmail.com>
2025-11-04 12:54:46 +0900

    音源のアップロードができるように実装

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,53 @@
 class PostsController < ApplicationController
+  before_action :authenticate_user!, except: [:index, :show]
+  before_action :set_post, only: [:show, :edit, :update, :destroy]
+  before_action :authorize_user!, only: [:edit, :update, :destroy]
+
   def index
     @posts = Post.includes(:user).order(created_at: :desc)
+  end
+
+  def show; end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = current_user.posts.build(post_params)
+    if @post.save
+      redirect_to @post, notice: "投稿が完了しました。"
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def edit; end
+
+  def update
+    if @post.update(post_params)
+      redirect_to @post, notice: "投稿を更新しました。"
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @post.destroy
+    redirect_to posts_path, notice: "投稿を削除しました。"
+  end
+
+  private
+
+  def set_post
+    @post = Post.find(params[:id])
+  end
+
+  def authorize_user!
+    redirect_to posts_path, alert: "権限がありません。" unless @post.user == current_user
+  end
+
+  def post_params
+    params.require(:post).permit(:title, :description, :genre, :tempo, :audio)
   end
 end
