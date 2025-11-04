6fa2d7a6e0ecf919deb9e7ff568aa914623ad48b
kaito8958 <shimo.oka8958@gmail.com>
2025-11-04 16:29:17 +0900

    コメントフォームの追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
-  get 'comments/create'
-  get 'comments/destroy'
   devise_for :users
   root "posts#index"  
-  resources :posts
+
+  resources :posts do
+    resources :comments, only: [:create, :destroy]
+  end
 end
 
 
