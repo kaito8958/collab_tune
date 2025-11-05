c14e66876bc06fc978efe2fda317a85d1a5394f0
kaito8958 <shimo.oka8958@gmail.com>
2025-11-05 16:55:49 +0900

    コラボ承認、拒否機能の実装

--- app/controllers/collaborations_controller.rb
+++ app/controllers/collaborations_controller.rb
@@ -1,6 +1,12 @@
 class CollaborationsController < ApplicationController
   before_action :authenticate_user!
 
+  def index
+    @sent_collaborations = current_user.sent_collaborations.includes(:receiver, :post)
+    @received_collaborations = current_user.received_collaborations.includes(:requester, :post)
+  end
+
+
   def create
     @collaboration = current_user.sent_collaborations.build(collaboration_params)
     @collaboration.status = 'pending'
@@ -9,11 +15,22 @@ class CollaborationsController < ApplicationController
       redirect_to posts_path, alert: "自分の投稿には申請できません。"
       return
     end
-    
+
     if @collaboration.save
       redirect_to @collaboration.post, notice: 'コラボ申請を送信しました。'
     else
       redirect_to @collaboration.post, alert: 'コラボ申請に失敗しました。'
+    end
+  end
+
+  def update
+  @collaboration = Collaboration.find(params[:id])
+
+    if params[:status].in?(%w[accepted rejected])
+      @collaboration.update(status: params[:status])
+      redirect_to collaborations_path, notice: "コラボ申請を#{@collaboration.status == 'accepted' ? '承認' : '拒否'}しました。"
+    else
+      redirect_to collaborations_path, alert: "無効な操作です。"
     end
   end
 
--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
   resources :posts do
     resources :comments, only: [:create, :destroy]
   end
-  resources :collaborations, only: [:create]
+  resources :collaborations, only: [:create, :index, :update]
 end
 
 
