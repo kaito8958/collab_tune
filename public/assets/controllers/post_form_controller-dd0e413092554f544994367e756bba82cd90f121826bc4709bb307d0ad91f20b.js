ee950972164b496e5d0efaa5ccd692b78e1c9870
kaito8958 <shimo.oka8958@gmail.com>
2025-11-20 13:14:42 +0900

    .gitignoreを編集

---
+++ public/assets/controllers/post_form_controller-dd0e413092554f544994367e756bba82cd90f121826bc4709bb307d0ad91f20b.js
@@ -0,0 +1,17 @@
+// app/javascript/controllers/post_form_controller.js
+import { Controller } from "@hotwired/stimulus"
+
+// Connects to data-controller="post-form"
+export default class extends Controller {
+  static targets = ["recruitFields", "statusSelect"]
+
+  connect() {
+    this.toggleRecruitFields()
+  }
+
+  toggleRecruitFields() {
+    const status = this.statusSelectTarget.value
+    const isRecruiting = status === "recruiting"
+    this.recruitFieldsTarget.classList.toggle("hidden", !isRecruiting)
+  }
+};
