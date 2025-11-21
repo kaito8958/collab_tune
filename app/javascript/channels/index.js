668975ce6b7dbbb9c54f3334cc399eacb9c9b35d
kaito8958 <shimo.oka8958@gmail.com>
2025-11-21 22:11:36 +0900

    Update index.js

--- app/javascript/channels/index.js
+++ app/javascript/channels/index.js
@@ -1,4 +1,5 @@
 // Import all the channels to be used by Action Cable
+console.log("📦 channels/index.js loaded");
 import "channels/consumer"
 import "channels/message_channel"
 import "channels/notification_channel"
