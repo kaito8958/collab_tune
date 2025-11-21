2e5f3c76bb885f28b46779e97ccab8c5800783f7
kaito8958 <shimo.oka8958@gmail.com>
2025-11-21 22:00:00 +0900

    Update importmap.rb

--- config/importmap.rb
+++ config/importmap.rb
@@ -4,11 +4,20 @@ pin "application"
 pin "@hotwired/turbo-rails", to: "turbo.min.js"
 pin "@hotwired/stimulus", to: "stimulus.min.js"
 pin "@hotwired/stimulus-loading", to: "stimulus-loading.js"
+
 pin_all_from "app/javascript/controllers", under: "controllers"
-pin "channels/message_channel"
-pin "channels/notification_channel"
+
+# ---- ActionCable（channels）ここが本命 ----
+# 個別 pin は要らないので削除
+# pin "channels/message_channel"
+# pin "channels/notification_channel"
 # pin "channels/consumer", to: "channels/consumer.js"
-# pin_all_from "app/javascript/channels", under: "channels"
+
+# 正しくはこれ1行だけ必要
+pin_all_from "app/javascript/channels", under: "channels"
+# -------------------------------------------
+
 pin "@rails/actioncable", to: "actioncable.esm.js"
+
 pin "header", to: "header.js"
 pin "audio_player", to: "audio_player.js"
