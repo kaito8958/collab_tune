8c7af55d77fad94aee6f2b481b0b20e86ba9a808
kaito8958 <shimo.oka8958@gmail.com>
2025-11-26 16:52:20 +0900

    commentモデルのテストコードを実装

--- spec/factories/comments.rb
+++ spec/factories/comments.rb
@@ -1,5 +1,8 @@
 FactoryBot.define do
   factory :comment do
-    
+    body { Faker::Lorem.sentence }
+
+    association :user
+    association :post
   end
 end
--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -1,5 +1,33 @@
 require 'rails_helper'
 
 RSpec.describe Comment, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'コメントの保存' do
+    let(:comment) { build(:comment) }
+
+    context '保存できる場合' do
+      it '全ての値が存在すれば保存できる' do
+        expect(comment).to be_valid
+      end
+    end
+
+    context '保存できない場合' do
+      it 'body が空だと保存できない' do
+        comment.body = ''
+        comment.valid?
+        expect(comment.errors.full_messages).to include('コメントを入力してください')
+      end
+
+      it 'user が紐づいていないと保存できない' do
+        comment.user = nil
+        comment.valid?
+        expect(comment.errors.full_messages).to include('ユーザーを入力してください')
+      end
+
+      it 'post が紐づいていないと保存できない' do
+        comment.post = nil
+        comment.valid?
+        expect(comment.errors.full_messages).to include('投稿を入力してください')
+      end
+    end
+  end
 end
