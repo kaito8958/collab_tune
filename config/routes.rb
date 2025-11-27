230da88b89ae2806d27f70dd8cfe8f4343938aa3
kaito8958 <shimo.oka8958@gmail.com>
2025-11-27 14:25:16 +0900

    リファクタリング

--- config/routes.rb
+++ config/routes.rb
@@ -1,23 +1,19 @@
 Rails.application.routes.draw do
-  get 'users/show'
-  get 'messages/create'
-  get 'chat_rooms/index'
-  get 'chat_rooms/show'
-  get 'chat_rooms/create'
   get '/uptime', to: 'application#uptime'
   devise_for :users
+
+  root "posts#index"
+
   resources :users, only: [:show] do
     member do
-      get :posts   # ← 追加（/users/:id/posts）
+      get :posts   # /users/:id/posts
     end
   end
-
-  root "posts#index"  
 
   resources :posts do
     resources :comments, only: [:create, :destroy]
   end
-  
+
   resources :collaborations, only: [:create, :index, :update]
 
   resources :chat_rooms, only: [:index, :show, :create] do
